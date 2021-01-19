b8e54bece6e02a594b55dfcdbe495a022c3c7faa
Vincent <thisoneis4business@gmail.com>
2021-01-19 20:00:23 +0700

    Added back Vector2
    idk why this version is not using changes I made a while back, so I re-add the changes now the game code looks a little bit nicer and a bit more efficient

--- games/game.go
+++ games/game.go
@@ -2,7 +2,6 @@ package games
 
 import (
 	"math/rand"
-	"strings"
 )
 
 var (
@@ -16,44 +15,47 @@ var (
 // Sokoban Structure
 type Sokoban struct {
 	state   [][]string
-	player  []int
-	targets [][]int
-	items   [][]int
-	isWin   bool
+	player  Vector2
+	targets []Vector2
+	items   []Vector2
+	IsWin   bool
 }
 
 // Constructor
 func NewSokoban(width int, height int) *Sokoban {
 	var game = &Sokoban{
 		state:   nil,
-		player:  nil,
+		player:  Vector2{
+			x: 0,
+			y: 0,
+		},
 		targets: nil,
 		items:   nil,
-		isWin:   false,
+		IsWin:   false,
 	}
 
 	var numObj = rand.Intn(2) + 3
 
 	// Setup targets and item keys
 	for i := 0; i < numObj; i++ {
-		var target = []int{rand.Intn(height), rand.Intn(width)}
-		var item = []int{rand.Intn(height-2) + 1, rand.Intn(width-2) + 1}
+		var (
+			target = Vector2{rand.Intn(width), rand.Intn(height)}
+			item = Vector2{rand.Intn(width-2) + 1, rand.Intn(height-2) + 1}
+		)
+
 		game.targets = append(game.targets, target)
-
-		for isIn(game.items, item) || isIn(game.targets, item) {
-			item = []int{rand.Intn(height-2) + 1, rand.Intn(width-2) + 1}
+		for IsIn(game.items, item) || IsIn(game.targets, item) {
+			item = Vector2{rand.Intn(width-2) + 1, rand.Intn(height-2) + 1}
 		}
-
 		game.items = append(game.items, item)
 	}
 
 	// Instantiate the player
-	var player = []int{rand.Intn(height), rand.Intn(width)}
-	for isIn(game.items, player) || isIn(game.targets, player) {
-		player = []int{rand.Intn(height), rand.Intn(width)}
+	var player = Vector2{rand.Intn(width), rand.Intn(height)}
+	for IsIn(game.items, player) || IsIn(game.targets, player) {
+		player = Vector2{rand.Intn(width), rand.Intn(height)}
 	}
-	game.player = []int{player[0], player[1]}
-
+	game.player = player
 	game.state = fillGrid(width, height, player, game.targets, game.items)
 
 	return game
@@ -61,111 +63,72 @@ func NewSokoban(width int, height int) *Sokoban {
 
 // Methods
 func (game *Sokoban) Move(direction string) {
-	if game.isWin {
+	if game.IsWin {
 		return
 	}
 
-	var forecast = []int{game.player[0], game.player[1]}
-
-	// Made the changes aka the prediction
-	switch strings.ToLower(direction) {
-	case "w":
-		forecast[0] -= 1
-	case "s":
-		forecast[0] += 1
-	case "a":
-		forecast[1] -= 1
-	case "d":
-		forecast[1] += 1
-	}
+	var forecast = game.player.ApplyMovement(DirectionKeyCode[direction])
 
 	// Boundaries
-	if forecast[0] < 0 || forecast[0] > len(game.state)-1 {
+	if forecast.y < 0 || forecast.y > len(game.state)-1 {
 		return
 	}
-	if forecast[1] < 0 || forecast[1] > len(game.state[forecast[0]])-1 {
+	if forecast.x < 0 || forecast.x > len(game.state[forecast.y])-1 {
 		return
 	}
 
 	// Check next item in the forecast
-	var nextSpot = game.state[forecast[0]][forecast[1]]
+	var nextSpot = game.state[forecast.y][forecast.x]
 
-	if nextSpot == dead || nextSpot == target {
+	if nextSpot == dead || nextSpot == target || (nextSpot == bomb && !game.MoveObject(forecast, direction)){
 		return
-	}
-
-	// Recursive movement
-	if nextSpot == bomb {
-		var hasMoved = game.MoveObject(forecast, direction)
-		if !hasMoved {
-			return
-		}
 	}
 
 	// Switch the new location with the previous, basically move the character
-	var temp = game.state[game.player[0]][game.player[1]]
-	game.state[game.player[0]][game.player[1]] = empty
-	game.state[forecast[0]][forecast[1]] = temp
+	var temp = game.state[game.player.y][game.player.x]
+	game.state[game.player.y][game.player.x] = empty
+	game.state[forecast.y][forecast.x] = temp
 
 	// Update the coordinates
-	game.player = []int{forecast[0], forecast[1]}
+	game.player = Vector2{forecast.x, forecast.y}
 
-	game.isWin = len(game.targets) == 0
+	game.IsWin = len(game.targets) == 0
 }
 
-func (game *Sokoban) MoveObject(location []int, direction string) bool {
-	var forecast = []int{location[0], location[1]}
-
-	// Made the changes aka prediction
-	switch strings.ToLower(direction) {
-	case "w":
-		forecast[0] -= 1
-	case "s":
-		forecast[0] += 1
-	case "a":
-		forecast[1] -= 1
-	case "d":
-		forecast[1] += 1
-	}
+func (game *Sokoban) MoveObject(location Vector2, direction string) bool {
+	var forecast = game.player.ApplyMovement(DirectionKeyCode[direction])
 
 	// Boundaries
-	if forecast[0] < 0 || forecast[0] > len(game.state)-1 {
+	if forecast.y < 0 || forecast.y > len(game.state)-1 {
 		return false
 	}
-	if forecast[1] < 0 || forecast[1] > len(game.state[forecast[0]])-1 {
+	if forecast.x < 0 || forecast.x > len(game.state[forecast.y])-1 {
 		return false
 	}
 
 	// Check next item in the forecast
-	var nextSpot = game.state[forecast[0]][forecast[1]]
-	var object = bomb
+	var (
+		nextSpot = game.state[forecast.y][forecast.x]
+		object = bomb
+	)
 
-	// Not stack on top finished target
-	if nextSpot == dead {
+	// Not stack on top finished target, or recurse
+	if nextSpot == dead || (nextSpot == bomb && !game.MoveObject(forecast, direction)) {
 		return false
-	}
-
-	// Recursive movement
-	if nextSpot == bomb {
-		var hasMoved = game.MoveObject(forecast, direction)
-		if !hasMoved {
-			return false
-		}
 	}
 
 	if nextSpot == target {
 		object = dead
-		game.targets = remove(game.targets, forecast)
+		game.targets = Remove(game.targets, forecast)
 	}
 
 	// Made the movement
-	game.state[forecast[0]][forecast[1]] = object
-	game.state[location[0]][location[1]] = empty
+	game.state[forecast.y][forecast.x] = object
+	game.state[location.y][location.x] = empty
 
 	for i := 0; i < len(game.items); i++ {
-		var curr = game.items[i]
-		if curr[0] == location[0] && curr[1] == location[1] {
-			game.items[i] = []int{forecast[0], forecast[1]}
+		if game.items[i].Equal(location) {
+			game.items[i] = Vector2{ forecast.x, forecast.y }
 		}
 	}
 
@@ -173,24 +136,24 @@ func (game *Sokoban) MoveObject(location []int, direction string) bool {
 }
 
 func (game *Sokoban) Show() string {
-	return show(game.state, game.isWin)
+	return show(game.state, game.IsWin)
 }
 
 // Helper function
-func fillGrid(width int, height int, player []int, targets [][]int, items [][]int) [][]string {
+func fillGrid(width int, height int, player Vector2, targets []Vector2, items []Vector2) [][]string {
 	// Create an empty 2D Grid
 	var grid [][]string
 	for i := 0; i < height; i++ {
-		var row []string // Create an empty row
 
 		// For each column per row, insert an emoji depending on the coordinates given
+		var row []string
 		for j := 0; j < width; j++ {
-			var coords = []int{i, j}
-			if player[0] == i && player[1] == j {
+			var coords = Vector2{j, i}
+			if player.Equal(coords) {
 				row = append(row, fox)
-			} else if isIn(targets, coords) {
+			} else if IsIn(items, coords) {
 				row = append(row, target)
-			} else if isIn(items, coords) {
+			} else if IsIn(targets, coords) {
 				row = append(row, bomb)
 			} else {
 				row = append(row, empty)
@@ -203,30 +166,11 @@ func fillGrid(width int, height int, player []int, targets [][]int, items [][]in
 	return grid
 }
 
-func isIn(array [][]int, item []int) bool {
-	for i := 0; i < len(array); i++ {
-		if item[0] == array[i][0] && item[1] == array[i][1] {
-			return true
-		}
-	}
-	return false
-}
-
-func remove(array [][]int, item []int) [][]int {
-	for i := 0; i < len(array); i++ {
-		if array[i][0] == item[0] && array[i][1] == item[1] {
-			return append(array[:i], array[i+1:]...)
-		}
-	}
-	return array
-}
-
+// View Functions
 func show(grid [][]string, isWin bool) string {
 	// Choose a random color unless game has ended
 	var color = []string{"🟧", "🟪", "🟥"}[rand.Intn(3)]
-	if isWin {
-		color = "🟩"
-	}
+	if isWin { color = "🟩" }
 
 	// Draw the grid state
 	var res = wall(len(grid[0]), color) + "\n"
@@ -244,7 +188,6 @@ func show(grid [][]string, isWin bool) string {
 	} else {
 		res += "To move, send W A S D"
 	}
-
 	return res
 }
 
--- games/vectors.go
+++ games/vectors.go
@@ -4,14 +4,63 @@ type Vector2 struct {
 	x, y int
 }
 
+type Vector2Direction string
+
+const (
+	up Vector2Direction = "up"
+	down Vector2Direction = "down"
+	left Vector2Direction = "left"
+	right Vector2Direction = "right"
+)
+
+var (
+	DirectionKeyCode = map[string] Vector2Direction {
+		"w": up,
+		"a": left,
+		"s": down,
+		"d": right,
+	}
+)
 
 func (vec Vector2) Add(other Vector2) Vector2 {
-	return Vector2{
-		x: vec.x + other.x,
-		y: vec.y + other.y,
-	}
+	return Vector2{ vec.x + other.x,vec.y + other.y}
 }
 
 func (vec Vector2) Equal(other Vector2) bool {
 	return vec.x == other.x && vec.y == other.y
+}
+
+func (vec Vector2) ApplyMovement(direction Vector2Direction) Vector2 {
+	var (
+		x, y = vec.x, vec.y
+	)
+	switch direction {
+	case up:
+		y -= 1
+	case down:
+		y += 1
+	case left:
+		x -= 1
+	case right:
+		x += 1
+	}
+	return Vector2{x,y}
+}
+
+func IsIn(arr []Vector2, item Vector2) bool {
+	for i := 0; i < len(arr); i++ {
+		if arr[i].Equal(item) {
+			return true
+		}
+	}
+	return false
+}
+
+func Remove(arr []Vector2, item Vector2) []Vector2 {
+	for i := 0; i < len(arr); i++ {
+		if arr[i].x == item.x && arr[i].y == item.y {
+			return append(arr[:i], arr[i+1:]...)
+		}
+	}
+	return arr
 }
